b8436867267fc9eab432ee3d60db220639536b2f
cho960422 <87304487+cho960422@users.noreply.github.com>
2023-11-30 21:03:54 +0900

    날씨정보를 불러와 종합한 후 반환하는 코드

--- app/src/main/java/com/hk/portfolio/today_weather/core/util/WeatherUtil.kt
+++ app/src/main/java/com/hk/portfolio/today_weather/core/util/WeatherUtil.kt
@@ -5,25 +5,89 @@ import androidx.annotation.RequiresApi
 import com.hk.portfolio.today_weather.core.WeatherConditionEnum
 import com.hk.portfolio.today_weather.domain.entity.weather.WeatherConditionEntity
 import com.hk.portfolio.today_weather.domain.entity.weather.WeatherEntity
+import java.time.LocalDate
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 object WeatherUtil {
     @RequiresApi(Build.VERSION_CODES.O)
     private val EARLY_TIME_START = LocalTime.of(8, 0)
+
     @RequiresApi(Build.VERSION_CODES.O)
     private val MIDDLE_TIME_START = LocalTime.of(16, 0)
+
     @RequiresApi(Build.VERSION_CODES.O)
     private val NIGHT_TIME_START = LocalTime.of(0, 0)
+
     @RequiresApi(Build.VERSION_CODES.O)
     fun exportWeatherCondition(
         list: List<WeatherEntity>
-    ): WeatherConditionEntity {
+    ): WeatherConditionEnum {
         val resultMap = resultOfRainSnow(list)
         val rainArr = resultMap[WeatherConditionEnum.Rain]
         val snowArr = resultMap[WeatherConditionEnum.Snow]
         val rainSnow = resultMap[WeatherConditionEnum.RainOrSnow]
 
-//        val description =
+        return weatherPerDate(rainSize = rainArr?.size?: 0, snowSize = snowArr?.size?: 0, rainSnowSize = rainSnow?.size?:0)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun exportDescription(
+        list: List<WeatherEntity>
+    ): String {
+        val resultMap = resultOfRainSnow(list)
+        val rainArr = resultMap[WeatherConditionEnum.Rain]
+        val snowArr = resultMap[WeatherConditionEnum.Snow]
+        val rainSnow = resultMap[WeatherConditionEnum.RainOrSnow]
+        val rainStr = if (rainArr?.isNotEmpty() == true) rainArr.joinToString(", "){
+            DateTimeFormatter.ofPattern("HH:mm").format(it)
+        } + "에 비" else null
+        val snowStr = if (snowArr?.isNotEmpty() == true) snowArr.joinToString(", "){
+            DateTimeFormatter.ofPattern("HH:mm").format(it)
+        } + "에 는" else null
+        val rainSnowStr = if (rainSnow?.isNotEmpty() == true) rainSnow.joinToString(", "){
+            DateTimeFormatter.ofPattern("HH:mm").format(it)
+        } + "에 는/비" else null
+        val strArr = mutableListOf<String>()
+
+        if (rainStr != null) strArr.add(rainStr)
+        if (snowStr != null) strArr.add(snowStr)
+        if (rainSnowStr != null) strArr.add(rainSnowStr)
+
+        return if (strArr.isEmpty()) "눈/비 예보가 없어요" else strArr.joinToString(",\n") + " 예보가 있어요"
+    }
+
+    private fun weatherPerDate(
+        rainSize: Int,
+        snowSize: Int,
+        rainSnowSize: Int,
+    ): WeatherConditionEnum {
+        if (rainSize == snowSize && rainSize == rainSnowSize)
+            return WeatherConditionEnum.RainOrSnow
+
+        val map = HashMap<WeatherConditionEnum, Int>()
+        val sortList = mutableListOf(
+            WeatherConditionEnum.Rain,
+            WeatherConditionEnum.Snow,
+            WeatherConditionEnum.RainOrSnow
+        )
+
+        map[WeatherConditionEnum.Rain] = rainSize
+        map[WeatherConditionEnum.Snow] = snowSize
+        map[WeatherConditionEnum.RainOrSnow] = rainSnowSize
+
+        for (i in 0 until sortList.size - 1) {
+            for (j in i until sortList.size - 1) {
+                if ((map[sortList[j]]?: 0) < (map[sortList[j + 1]]?:0)) {
+                    val current = sortList[j]
+                    val next = sortList[j + 1]
+                    sortList[j] = next
+                    sortList[j+1] = current
+                }
+            }
+        }
+
+        return sortList.first()
     }
 
     /**
@@ -52,11 +116,11 @@ object WeatherUtil {
         resultMap[WeatherConditionEnum.Snow] = mutableListOf()
         resultMap[WeatherConditionEnum.RainOrSnow] = mutableListOf()
         list.forEach {
-            if (rainSet[WeatherConditionEnum.Rain] == true){
+            if (rainSet[WeatherConditionEnum.Rain] == true) {
                 resultMap[WeatherConditionEnum.Rain]?.add(it.dateTime.toLocalTime())
-            } else if (rainSet[WeatherConditionEnum.Snow] == true){
+            } else if (rainSet[WeatherConditionEnum.Snow] == true) {
                 resultMap[WeatherConditionEnum.Snow]?.add(it.dateTime.toLocalTime())
-            } else if (rainSet[WeatherConditionEnum.RainOrSnow] == true){
+            } else if (rainSet[WeatherConditionEnum.RainOrSnow] == true) {
                 resultMap[WeatherConditionEnum.RainOrSnow]?.add(it.dateTime.toLocalTime())
             }
         }
--- app/src/main/java/com/hk/portfolio/today_weather/domain/usecase/weather/GetWeatherUseCase.kt
+++ app/src/main/java/com/hk/portfolio/today_weather/domain/usecase/weather/GetWeatherUseCase.kt
@@ -1,24 +1,41 @@
 package com.hk.portfolio.today_weather.domain.usecase.weather
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.hk.portfolio.today_weather.core.util.WeatherUtil
 import com.hk.portfolio.today_weather.domain.entity.weather.WeatherConditionEntity
 import com.hk.portfolio.today_weather.domain.entity.weather.WeatherEntity
 import com.hk.portfolio.today_weather.domain.repository.WeatherRepository
 import com.hk.portfolio.today_weather.domain.usecase.CoroutineUseCase
+import java.time.LocalDate
 import java.time.LocalDateTime
 import javax.inject.Inject
 
 class GetWeatherUseCase @Inject constructor(
     private val weatherRepository: WeatherRepository,
 ): CoroutineUseCase<GetWeatherUseCase.Request, WeatherConditionEntity> {
-    class Request(val nx:Double, val ny: Double, val baseDateTime: LocalDateTime, val count:Int)
+    class Request(val eventId: String, val nx:Double, val ny: Double, val baseDateTime: LocalDateTime, val count:Int, val date: LocalDate)
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun invoke(param: Request): WeatherConditionEntity {
         val response = with(param){
             weatherRepository.getWeather(
                 nx, ny, baseDateTime, count
             )
         }
+        val filterArr = response.filter { it.dateTime.toLocalDate() == param.date }
 
-
+        val weatherCondition = WeatherUtil.exportWeatherCondition(filterArr)
+        val description = WeatherUtil.exportDescription(filterArr)
+        return WeatherConditionEntity(
+            id = param.eventId,
+            nx = param.nx,
+            ny = param.ny,
+            baseDateTime = param.baseDateTime,
+            weatherCondition = weatherCondition,
+            description = description,
+            eventLocationId = param.eventId,
+            date = param.date
+        )
     }
 }
